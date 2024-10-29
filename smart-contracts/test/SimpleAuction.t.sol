b97d37f7efbaa6cd26393498095953368fc777e2
najienka <nemitariajienka@gmail.com>
2024-10-29 16:11:07 +0000

    Add test for FulfillHighestBid

--- smart-contracts/test/SimpleAuction.t.sol
+++ smart-contracts/test/SimpleAuction.t.sol
@@ -15,16 +15,17 @@ contract SimpleAuctionTest is Test {
     address bidder2;
 
     function setUp() public {
-        auctioneer = address(this); // Use the test contract as auctioneer
+        auctioneer = vm.addr(1); // Use the test contract as auctioneer
+        bidder1 = vm.addr(2);
+        bidder2 = vm.addr(3);
 
-        bidder1 = vm.addr(1);
-        bidder2 = vm.addr(2);
-
+        vm.startPrank(auctioneer);
         auction = new SimpleAuction(durationBlocks, reservePrice, highestBidPaymentWindowBlocks);
+        vm.stopPrank();
     }
 
     function test_DeploymentConfigurations() public view {
-        assertEq(auction.auctioneer(), address(this));
+        assertEq(auction.auctioneer(), auctioneer);
         assertGt(auction.auctionEndBlock(), block.number);
         assertEq(auction.highestBidPaymentDeadlineBlock(), auction.auctionEndBlock() + highestBidPaymentWindowBlocks);
     }
@@ -60,5 +61,29 @@ contract SimpleAuctionTest is Test {
         assertEq(auction.highestBidder(), bidder1, "Highest bidder should be bidder1");
         assertEq(b.bidder, bidder1, "Bidder for bid ID 1 should be bidder1");
         assertEq(auction.highestBid(), bidAmount, "Highest bid should be 1000");
+    }
+
+    function test_FulfillHighestBid() public {
+        // Bidder1 places a bid
+        uint256 bidAmount = 0.5 ether;
+        vm.deal(bidder1, 1 ether);
+        vm.startPrank(bidder1);
+        // todo only .prank(addr) not working for some reason??
+        // vm.prank(bidder1);
+        uint256 bidID = auction.sealedBid{value: auction.reservePrice()}(abi.encodePacked(bidAmount)); // Place a sealed bid of bidAmount
+        vm.stopPrank();
+
+        // Move to the auction end block to end the auction
+        vm.roll(auction.auctionEndBlock() + 1);
+        
+        // Reveal the bid
+        auction.revealBid(bidID, bidAmount); // Reveal the bid
+        
+        // Bidder1 fulfills the highest bid
+        vm.startPrank(bidder1); // Set bidder1 as the sender
+        auction.fulfilHighestBid{value: bidAmount - auction.reservePrice()}();
+        vm.stopPrank();
+        
+        assert(auction.highestBidPaid());
     }
 }
